381bbb5544c926e9f603e82d3ac46531bcf07102
Louis-Francois Meunier <louis-francois.meunier@meteo.fr>
2021-02-17 15:25:05 +0100

    0.4 release

--- src/tflowclient/__init__.py
+++ src/tflowclient/__init__.py
@@ -62,6 +62,6 @@ Here are a few pointers for a better understanding of the code:
 
 __all__ = ["cdp_flow", "demo_flow", "TFlowApplication"]
 
-__version__ = "0.3.3"
+__version__ = "0.4.0"
 
 from .ui import TFlowApplication
