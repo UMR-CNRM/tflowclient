c992ef236918cb9ba5b55aa94b55bb6a8b20d26e
Louis-Francois Meunier <louis-francois.meunier@meteo.fr>
2021-09-07 17:51:45 +0200

    Bump version to 0.5.1

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def readme():
 
 setup(
     name="tflowview",
-    version="0.5.0",
+    version="0.5.1",
     description="A text-based viewer for ECMWF workflow schedulers.",
     long_description=readme(),
     classifiers=[
