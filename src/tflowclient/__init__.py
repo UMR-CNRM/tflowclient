46fabe2ad4f369ca1a06b81349ea312ad8194a26
Louis-Francois Meunier <louis-francois.meunier@meteo.fr>
2023-03-17 17:35:28 +0100

    Remove version number from the code

--- src/tflowclient/__init__.py
+++ src/tflowclient/__init__.py
@@ -60,6 +60,4 @@ Here are a few pointers for a better understanding of the code:
 
 __all__ = ["cdp_flow", "demo_flow", "TFlowApplication"]
 
-__version__ = "0.6.1"
-
 from .ui import TFlowApplication
